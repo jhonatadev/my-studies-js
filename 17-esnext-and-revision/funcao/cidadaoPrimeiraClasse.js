a93be9dfd9a7d7581e70399b4936b16cca123f4a
xxxonata <xxxonata@gmail.com>
2019-05-30 13:29:05 -0300

    revision function and obj

---
+++ 17-esnext-and-revision/funcao/cidadaoPrimeiraClasse.js
@@ -0,0 +1,33 @@
+// Função em JS é First-Class Object (Citinzens)
+// Higher-oder function
+
+// criar de forma literal
+function fun1() { } 
+
+// Armazenar em uma variável
+const fun2 = function() {}
+
+// Armazenar em um array
+const array = [function (a, b) { return a + b }, fun1, fun2]
+console.log(array[0](2, 3));
+
+// Armazenar em um atributo de objeto
+const obj = {}
+obj.falar = function() { return 'Opa' }
+console.log(obj.falar())
+
+// Pssar função como param
+function run(fun) {
+  fun()
+}
+
+run(function () { console.log('Executando....') } )
+
+// Um função pode retornar/conter uma função
+function soma(a, b) {
+  return function (c) {
+    console.log(a + b + c);
+  }
+}
+
+soma(2, 3)(4) 
